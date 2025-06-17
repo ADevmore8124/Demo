6e9c9367b0cbafbce048061eb1b7bba4949fc257
ADMIN <ADMIN@DESKTOP-3UIE6OA>
2025-06-17 19:30:16 +0530

    Add project files.

---
+++ Demo/Account.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace Demo
+{
+    internal class Account
+    {
+        private int CN, pin;
+        private double CB = 2000; // Current balance
+        private double SB = 1000; // Saving balance
+        private double amount;
+
+        public void setCustomerNumber(int cn)
+        {
+            CN = cn;
+        }
+
+        public void setCustomerPin(int p)
+        {
+            pin = p;
+        }
+
+        public int getCustomerNumber()
+        {
+            return CN;
+        }
+
+        public int getCustomerPin()
+        {
+            return pin;
+        }
+
+        public string getCurrentBalance()
+        {
+            return CB.ToString("0.00");
+        }
+
+        public string getSavingBalance()
+        {
+            return SB.ToString("0.00");
+        }
+
+        public void getCurrentWithdrawInput()
+        {
+            Console.WriteLine("Your Current Balance: ₹" + CB);
+            Console.Write("Enter the amount to withdraw: ₹");
+            amount = Convert.ToDouble(Console.ReadLine());
+
+            if ((CB - amount) >= 0)
+            {
+                CB -= amount;
+                Console.WriteLine("✅ Transaction Successful.");
+                Console.WriteLine("New Current Balance: ₹" + CB);
+            }
+            else
+            {
+                Console.WriteLine("❌ Insufficient balance.");
+            }
+        }
+
+        public void getSavingWithdrawInput()
+        {
+            Console.WriteLine("Your Saving Balance: ₹" + SB);
+            Console.Write("Enter the amount to withdraw: ₹");
+            amount = Convert.ToDouble(Console.ReadLine());
+
+            if ((SB - amount) >= 0)
+            {
+                SB -= amount;
+                Console.WriteLine("✅ Transaction Successful.");
+                Console.WriteLine("New Saving Balance: ₹" + SB);
+            }
+            else
+            {
+                Console.WriteLine("❌ Insufficient balance.");
+            }
+        }
+
+        public void getCurrentDepositeInput()
+        {
+            Console.WriteLine("Your Current Balance: ₹" + CB);
+            Console.Write("Enter the amount to deposit: ₹");
+            amount = Convert.ToDouble(Console.ReadLine());
+
+            CB += amount;
+            Console.WriteLine("✅ Deposit Successful.");
+            Console.WriteLine("New Current Balance: ₹" + CB);
+        }
+
+        public void getSavingDepositInput()
+        {
+            Console.WriteLine("Your Saving Balance: ₹" + SB);
+            Console.Write("Enter the amount to deposit: ₹");
+            amount = Convert.ToDouble(Console.ReadLine());
+
+            SB += amount;
+            Console.WriteLine("✅ Deposit Successful.");
+            Console.WriteLine("New Saving Balance: ₹" + SB);
+        }
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Program
+    {
+        public static void m1()
+        {
+            int a = 20;
+            int b = 30;
+            Console.WriteLine("Addition is :" + (a + b));
+            Console.WriteLine("Substration is :" + (a - b));
+            Console.WriteLine("Multiplication is :" + (a * b));
+            Console.WriteLine("Division is :" + (a / b));
+
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            //inputData obj = new inputData();
+            //  obj.method1();
+            //   obj.method2();
+            //  obj.method4();
+
+            //Temperature t = new Temperature();
+            //t.inputTemp();
+
+            //simpleATM s = new simpleATM();
+            //s.atm();
+            
+            BillGenration b=new BillGenration();
+           b.calBill();
+            Console.ReadLine();
+        }
+    }
+}
